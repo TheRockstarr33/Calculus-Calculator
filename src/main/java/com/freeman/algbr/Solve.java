84d7bef1b721692d13834f3deb46b524b5e70abf
TheRockstarr33 <drockstarr33@yahoo.com>
2019-03-25 15:13:32 +0400

    Added quadratic and linear

--- src/main/java/com/freeman/algbr/Solve.java
+++ src/main/java/com/freeman/algbr/Solve.java
@@ -3,6 +3,10 @@ package com.freeman.algbr;
 import com.freeman.calc.Derivative;
 import com.freeman.obj.Polynomial;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Solve {
 
     //Can find approx by derivative when solving for extrema
@@ -54,5 +58,37 @@ public class Solve {
             System.out.println("UNCERTAIN: ");
             return x;
         }
+    }
+
+    /**
+     * Solves a quadratic equation, only works when 0.0, 1.0, and
+     * 2.0 are defined in the Polynomial map.
+     *
+     * @param p
+     * @return
+     */
+    public static List<Double> solveForQuadratic(Polynomial p) {
+        Map<Double, Double> m = p.getTerms();
+        if(m.get(2.0)!=null) {
+            return solveForXQuadratic(m.get(2.0), m.get(1.0), m.get(0.0));
+        } else {
+            return solveForXLinear(m.get(1.0), m.get(0.0));
+        }
+    }
+
+    public static List<Double> solveForXQuadratic(double a, double b, double c) {
+        List<Double> resultList = new ArrayList<Double>();
+        double temp1 = Math.sqrt(b * b - 4 * a * c);
+        double root1 = (-b + temp1) / (2 * a);
+        double root2 = (-b - temp1) / (2 * a);
+        resultList.add(root1);
+        resultList.add(root2);
+        return resultList;
+    }
+
+    public static List<Double>  solveForXLinear(double b, double c) {
+        List<Double> resultList = new ArrayList<Double>();
+        resultList.add(-c / b);
+        return resultList;
     }
 }
--- src/test/java/SolveTest.java
+++ src/test/java/SolveTest.java
@@ -1,30 +1,29 @@
 import com.freeman.algbr.Solve;
-import com.freeman.obj.Polynomial;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class SolveTest {
 
     @Test
-    public void isSolveForXForZeroOk() {
-        Map m1 = new HashMap();
-        m1.put(0.0, 0.0);
-        Polynomial p1 = new Polynomial(m1);
+    public void isSolveForXQuadraticOk() {
+        List<Double> expectedList = Solve.solveForXQuadratic(2.0, 6.0, 4.0);
+        List<Double> actualList = new ArrayList<Double>();
+        actualList.add(-1.0);
+        actualList.add(-2.0);
 
-        assertEquals(Solve.solveForX(p1, 0.0), 0.0);
+        assertEquals(actualList, expectedList);
     }
 
     @Test
-    public void isSolveForXForNonZeroOk() {
-        Map m1 = new HashMap();
-        m1.put(4.0, 0.0);
-        m1.put(5.0, 2.0);
-        Polynomial p1 = new Polynomial(m1);
+    public void isSolveForXLinearOk() {
+        List<Double> expectedList = Solve.solveForXLinear(4.0, 6.0);
+        List<Double> actualList = new ArrayList<Double>();
+        actualList.add(-1.5);
 
-        assertEquals(2.0, Solve.solveForX(p1, 24.0));
+        assertEquals(actualList, expectedList);
     }
 }
